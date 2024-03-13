c75e99c68e982350d45045d984410e58fe1baf73
morit <moritzhecht@gmx.net>
2024-03-13 14:31:00 +0100

    added floor data interface in modal

--- components/modals/AddFloorData.tsx
+++ components/modals/AddFloorData.tsx
@@ -7,11 +7,31 @@ import Modal from "@/components/layout/Modal";
 import {Language} from "@/constants/types";
 import styles from "@/styles/modals/AddFloorData.module.css";
 import {closeIsAddingFloorDataModalActive} from "@/store/reducer/isAddingFloorDataModalActive";
+import {ChangeEvent, useState} from "react";
 
 export default function AddFloorData({}: AddFloorDataModalProps) {
     const language: Language = de
     const state: RootState = useSelector((state: RootState) => state)
     const dispatch = useDispatch()
+
+    const [counterNumber, setCounterNumber] = useState('')
+    const [kilowattValue, setKilowattValue] = useState('')
+
+    const onCounterNumberChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const currentCounterNumber: number | undefined = parseInt(event.target.value)
+        if (currentCounterNumber && currentCounterNumber > 0)
+            setCounterNumber(currentCounterNumber.toString())
+        else
+            setCounterNumber('')
+    }
+
+    const onKilowattValueChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const currentKilowattValue: number | undefined = parseInt(event.target.value)
+        if (currentKilowattValue && currentKilowattValue > 0)
+            setKilowattValue(currentKilowattValue.toString())
+        else
+            setKilowattValue('')
+    }
 
     const onAbortClickHandler = () => {
         dispatch(closeIsAddingFloorDataModalActive())
@@ -19,6 +39,28 @@ export default function AddFloorData({}: AddFloorDataModalProps) {
 
     return (
         <Modal formName={'addFloorData'}>
+            <input value={counterNumber}
+                   className={`${styles.input} ${true ? styles.inputValid : styles.inputInvalid}`}
+                   type={"number"}
+                   min={0}
+                   max={999999}
+                   step={1.0}
+                   onChange={(e) => {
+                       onCounterNumberChange(e)
+                   }}
+                   placeholder={de.inputLabels.kilometer}
+            />
+            <input value={kilowattValue}
+                   className={`${styles.input} ${true ? styles.inputValid : styles.inputInvalid}`}
+                   type={"number"}
+                   min={0}
+                   max={999999}
+                   step={1.0}
+                   onChange={(e) => {
+                       onKilowattValueChange(e)
+                   }}
+                   placeholder={de.inputLabels.kilometer}
+            />
             <button onClick={onAbortClickHandler} className={styles.button}>{de.buttonLabels.abort}</button>
         </Modal>
     );
