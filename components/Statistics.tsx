97b1ebbc253e9d9ff7891acb408c70989249f38d
Moritz Hecht <moritz-hecht@gmx.net>
2026-02-06 16:35:02 +0100

    wip add better stats

--- components/Statistics.tsx
+++ components/Statistics.tsx
@@ -22,39 +22,87 @@ export default function Statistics({}: StatisticsProps) {
     const [kwhFueled, setKwhFueled] = useState(0)
     const [priceMultiplier, setPriceMultiplier] = useState("0.2")
     const [priceToPay, setPriceToPay] = useState(0)
-    const [currentDateValue, setCurrentDateValue] = useState<YearMonth>({
+    const [fromDateValue, setFromDateValue] = useState<YearMonth>({
         year: year.toString(),
         month: monthString
     })
+    const [toDateValue, setToDateValue] = useState<YearMonth>({
+        year: year.toString(),
+        month: monthString
+    })
+    const [analysisData, setAnalysisData] = useState<AnalysisItem[]>([])
+
+    type AnalysisItem = {
+        yearMonth: YearMonth,
+        kwhFueled: number,
+        priceToPay: number,
+        kilometersDriven: number,
+    }
 
     useEffect(() => {
-        getFullDataSet(state.currentCar.name, currentDateValue).then((dataSet) => {
-            if (dataSet && dataSet.length > 0)
-            {
-                let kwh = 0
-                dataSet.map((dataItem) => {
-                    kwh += parseFloat(dataItem.power.toString())
-                })
-                setKwhFueled(kwh)
-                if (isPowerValid(priceMultiplier))
-                    setPriceToPay(kwh * parseFloat(priceMultiplier) ?? 1)
-                setKilometersDriven(dataSet[0].kilometer - dataSet[dataSet.length-1].kilometer)
-            } else {
-                setKwhFueled(0)
-                setPriceToPay(0)
-                setKilometersDriven(0)
-            }
-        }).catch((e: Error) => console.log(e.message))
-    }, [state.currentCar.name, currentDateValue, priceMultiplier]);
+        const analysisItems: AnalysisItem[] = []
+        let y = Number(fromDateValue.year);
+        let m = Number(fromDateValue.month);
 
-    const onDateInputChangeHandler = async (event: ChangeEvent<HTMLInputElement>) => {
+        while (y <  Number(toDateValue.year) || (y ===  Number(toDateValue.year) && m <=  Number(toDateValue.month))) {
+            getFullDataSet(state.currentCar.name, { year: y.toString(), month: (m < 10 ? `0` + m : m).toString() }).then((dataSet) => {
+                let analysisItem: AnalysisItem = {
+                    yearMonth: { year: y.toString(), month: (m < 10 ? `0` + m : m).toString() },
+                    kwhFueled: 0,
+                    priceToPay: 0,
+                    kilometersDriven: 0,
+                }
+                if (dataSet && dataSet.length > 0)
+                {
+                    let kwh = 0
+                    dataSet.map((dataItem) => {
+                        kwh += parseFloat(dataItem.power.toString())
+                    })
+                    analysisItem.kwhFueled = kwh;
+                    // setKwhFueled(kwh)
+                    if (isPowerValid(priceMultiplier))
+                        // setPriceToPay(kwh * parseFloat(priceMultiplier) ?? 1)
+                        analysisItem.priceToPay = kwh * parseFloat(priceMultiplier) ?? 1;
+                    // setKilometersDriven(dataSet[0].kilometer - dataSet[dataSet.length-1].kilometer)
+                    analysisItem.kilometersDriven = dataSet[0].kilometer - dataSet[dataSet.length-1].kilometer
+                } /*else {
+                    setKwhFueled(0)
+                    setPriceToPay(0)
+                    setKilometersDriven(0)
+                }*/
+                analysisItems.push(analysisItem)
+            }).catch((e: Error) => console.log(e.message))
+
+            m++;
+            if (m === 13) {
+                m = 1;
+                y++;
+            }
+        }
+
+        console.log(analysisItems)
+
+    }, [state.currentCar.name, fromDateValue, priceMultiplier, toDateValue]);
+
+    const onFromDateInputChangeHandler = async (event: ChangeEvent<HTMLInputElement>) => {
         if (event.target.value !== '') {
             const arrayDate: string[] = event.target.value.split('-')
             const yearMonth = {
                 year: arrayDate[0],
                 month: arrayDate[1]
             }
-            setCurrentDateValue(yearMonth)
+            setFromDateValue(yearMonth)
+        }
+    }
+
+    const onToDateInputChangeHandler = async (event: ChangeEvent<HTMLInputElement>) => {
+        if (event.target.value !== '') {
+            const arrayDate: string[] = event.target.value.split('-')
+            const yearMonth = {
+                year: arrayDate[0],
+                month: arrayDate[1]
+            }
+            setToDateValue(yearMonth)
         }
     }
 
@@ -75,13 +123,24 @@ export default function Statistics({}: StatisticsProps) {
                     </div>
                 : null}
                 <div className={styles.inputContainer}>
-                    <div>{de.inputLabels.date}:</div>
+                    <div>{de.inputLabels.from}:</div>
                     <input
-                        onChange={onDateInputChangeHandler}
-                        value={`${currentDateValue.year}-${currentDateValue.month}`}
+                        onChange={onFromDateInputChangeHandler}
+                        value={`${fromDateValue.year}-${fromDateValue.month}`}
                         className={globalStyles.monthPicker}
-                        style={{width: "10rem"}}
-                        type={"month"}/>
+                        style={{width: "12rem"}}
+                        type={"month"}
+                        max={``}/>
+                </div>
+                <div className={styles.inputContainer}>
+                    <div>{de.inputLabels.to}:</div>
+                    <input
+                        onChange={onToDateInputChangeHandler}
+                        value={`${toDateValue.year}-${toDateValue.month}`}
+                        className={globalStyles.monthPicker}
+                        style={{width: "12rem"}}
+                        type={"month"}
+                        max={``}/>
                 </div>
                 <div className={styles.inputContainer}>
                     <div>{de.inputLabels.priceMultiplier}:</div>
