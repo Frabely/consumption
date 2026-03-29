f6ef339c26dcb0bd272210292864eba1a5de4c41
Moritz Hecht <moritz-hecht@gmx.net>
2026-03-29 18:40:28 +0200

    add config for trial project

--- firebase/firebase.tsx
+++ firebase/firebase.tsx
@@ -27,6 +27,16 @@ const firebaseConfig = {
 //     appId: process.env.NEXT_PUBLIC_FIREBASE_DEV_APP_ID
 // }
 
+//trial
+// const firebaseConfig = {
+//     apiKey: process.env.NEXT_PUBLIC_FIREBASE_TRIAL_API_KEY,
+//     authDomain: process.env.NEXT_PUBLIC_FIREBASE_TRIAL_AUTH_DOMAIN,
+//     projectId: process.env.NEXT_PUBLIC_FIREBASE_TRIAL_PROJECT_ID,
+//     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_TRIAL_STORAGE_BUCKET,
+//     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_TRIAL_MESSAGING_SENDER_ID,
+//     appId: process.env.NEXT_PUBLIC_FIREBASE_TRIAL_APP_ID
+// }
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 export default app
